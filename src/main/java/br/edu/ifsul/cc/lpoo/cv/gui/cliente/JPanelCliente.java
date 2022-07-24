5daf6644baf6b77e460e8ae8a7311531e357b403
Paulo Henrique Silva <paulosilva.pf157@academico.ifsul.edu.br>
2022-07-24 16:27:51 -0300

    Corrigindo o JPanel Cliente, Listagem e Edicao

--- src/main/java/br/edu/ifsul/cc/lpoo/cv/gui/cliente/JPanelCliente.java
+++ src/main/java/br/edu/ifsul/cc/lpoo/cv/gui/cliente/JPanelCliente.java
@@ -1,49 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package br.edu.ifsul.cc.lpoo.cv.gui.cliente;
 
 import br.edu.ifsul.cc.lpoo.cv.Controle;
-
-import javax.swing.*;
-import java.awt.*;
+import java.awt.CardLayout;
+import javax.swing.JPanel;
 
 public class JPanelCliente extends JPanel {
-
+    
     private Controle controle;
     private CardLayout cardLayout;
-
-
+    
     private JPanelListagem telaListagem;
     private JPanelEdicao telaEdicao;
-
-
-    public JPanelCliente (Controle controle){
+    
+    
+    
+    
+    
+    public JPanelCliente(Controle controle){
         this.controle = controle;
-
+        
+        
         initComponents();
     }
-
-
-
+    
+    
     private void initComponents(){
-
-        cardLayout = new CardLayout(); //inicializando um novo gerenciador de layout
-        this.setLayout(cardLayout); //definindo o gerenciador de layout no painel
-
-        telaListagem = new JPanelListagem(this, controle);
-        telaEdicao = new JPanelEdicao(this, controle);
-
-
+        cardLayout = new CardLayout(); //inicializando o gerenciador
+        this.setLayout(cardLayout); //setando o gerenciador de layout
+        
+        
+        telaListagem = new JPanelListagem(this, controle); //inicializando a tela
+        telaEdicao = new JPanelEdicao(this, controle); //inicializando a tela
+        
         this.add(telaListagem, "tela_listagem"); //adicionando tela
         this.add(telaEdicao, "tela_edicao"); //adicionando tela
-
-
-        cardLayout.show(this,"tela_listagem"); //tela apresentada por padrao
-
-
-
-
-
-
-
+        
+        
+        cardLayout.show(this,"tela_listagem"); //mostra a tela listagem por padrao
+        
     }
-
+    
+    
+    
 }
